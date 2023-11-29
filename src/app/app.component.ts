6fdb45444d4b99bbe14853281348f0897df3798c
your_username <kamal1997khatri@gmail.com>
2023-11-29 22:15:26 +0545

    added the smooth scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent implements OnInit {
     ScrollSmoother.create({
       wrapper: ".main",
       content: ".main-container",
-      smooth: 1,
+      smooth: 1.5,
       effects: true,
     });
   }
