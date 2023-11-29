de22a9f675e9a25413c5e27da95021eeac474522
your_username <kamal1997khatri@gmail.com>
2023-11-29 22:14:29 +0545

    added the smooth scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  implements OnInit {
+export class AppComponent implements OnInit {
   isLoading: boolean = true;
 
   ngOnInit() {
@@ -14,5 +19,15 @@ export class AppComponent  implements OnInit {
       // Set isLoading to false to hide the loading component
       this.isLoading = false;
     }, 2000); // Adjust the time as needed
+
+
+    ScrollSmoother.create({
+      wrapper: ".main",
+      content: ".main-container",
+      smooth: 1,
+      effects: true,
+    });
   }
+
+
 }
