4211b954cb5bb0c14f7f937489444345f76e46ec
your_username <kamal1997khatri@gmail.com>
2023-11-29 23:20:21 +0545

    added animation to about us section

--- src/app/pages/home-page/about/about.component.ts
+++ src/app/pages/home-page/about/about.component.ts
@@ -1,5 +1,7 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'about',
@@ -8,23 +10,64 @@ import { gsap } from 'gsap';
 })
 export class AboutComponent implements AfterViewInit {
 
-  @ViewChild('sliderList', { static: true }) sliderList!: ElementRef<HTMLSpanElement>;
+  @ViewChild('about', { static: true }) about!: ElementRef;
+  @ViewChildren('aboutListItem') aboutListItems!: QueryList<ElementRef>;
 
   currentSlideIndex: number = 0;
 
   constructor() { }
-
   ngAfterViewInit() {
-    gsap.to(this.sliderList.nativeElement, {
-      duration: 1,
-      rotation: 360,
-      opacity: 1, 
-      delay: 0.5, 
-      stagger: 0.2,
-      ease: "sine.out", 
-      force3D: true
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: this.about.nativeElement,
+        start: "top center", // start the animation when "top" of the trigger hits the "center" of the viewport
+        end: "bottom center", // end the animation when "bottom" of the trigger hits the "center" of the viewport
+        toggleActions: "play none none none" // defines what happens to the animation at certain scroll points (play, pause, resume, reset)
+      },
+      defaults: { duration: .5 }
     });
+
+    const originalTextElement = this.about.nativeElement.querySelector('.section-title .title');
+    const originalText = originalTextElement ? originalTextElement.textContent : '';
+    const uniqueChars = Array.from(new Set(originalText.replace(/\s/g, ''))).join('');
+
+    // gsap
+
+    // Add sequential animations
+    tl.from(this.about.nativeElement.querySelector('.section-title .title-label'), { x: -100, opacity: 0 })
+      .from(this.about.nativeElement.querySelector('.section-title .title'), { x: -100, opacity: 0 }, '-=0.2')
+      .from(this.about.nativeElement.querySelector('.line img'), { width: 0, opacity: 0 }, '-=0.5')
+      .from(this.about.nativeElement.querySelector('.img-sm'), { scale: 0, opacity: 0 }, '-=0.5')
+      .from(this.about.nativeElement.querySelectorAll('.para'), { y: 100, opacity: 0, stagger: 0.2 })
+      .from(this.about.nativeElement.querySelector('.btn-trans'), { y: 100, opacity: 0, ease: 'elastic.out(1, 0.3)' })
+      .from(this.about.nativeElement.querySelector('.big-img'), { x: 100, opacity: 0, stagger: 0.2 })
+      .to(this.about.nativeElement.querySelector('.section-title .title'), {
+        duration: 2,
+        scrambleText: {
+          text: originalText,
+          chars: uniqueChars,
+          revealDelay: 0.2,
+          speed: 0.1,
+        }
+      })
+
+    this.aboutListItems.forEach((item, index) => {
+      gsap.from(item.nativeElement, {
+        duration: 0.5,
+        scale: 0.5,
+        opacity: 0,
+        delay: index * 0.2, // Delays each item's animation slightly more than the previous one
+        scrollTrigger: {
+          trigger: item.nativeElement,
+          start: "top bottom", // Animation starts when top of item hits the bottom of viewport
+          toggleActions: "play none none none"
+        }
+      });
+    });
+
   }
+
 
   aboutUsData = [
     {
